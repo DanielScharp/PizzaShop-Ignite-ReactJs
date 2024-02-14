27ebdac170575f453ab6a4f2157b14c3fb53d1be
Daniel Scharp <daniel.scharp@hotmail.com>
2024-02-14 16:22:41 -0300

    signIn and signUp

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './global.css'
 
 import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider, Helmet } from 'react-helmet-async';
+import { Toaster } from 'sonner'
 
 import { router } from './routes';
 
@@ -10,6 +11,7 @@ export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate='%s | pizza.shop' />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
